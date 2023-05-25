fedcde8a2f3e8419e08afac5b2d1ce2c9df8183a
Sushil <skshinde2028@gmail.com>
2023-05-25 09:28:14 +0530

    [Sushil] Uploading reflection

---
+++ Reflections/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections
+{
+    internal class Customer
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+
+        public Customer(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Customer()
+        {
+            this.age = 50;
+            this.name = "Sushil";
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"The name is {this.name} and age is {this.age}");
+
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"The first name is {this.name}");
+        }
+    }
+}
---
+++ Reflections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly executing = Assembly.GetExecutingAssembly();
+            Type[] types = executing.GetTypes();
+
+            foreach(Type type in types)
+            {
+                Console.WriteLine("Full name: {0}", type.FullName);
+                Console.WriteLine($"Class name: {type.Name}");
+
+                MethodInfo[] methods = type.GetMethods();
+                foreach(var method in methods)
+                {
+                    Console.WriteLine($"Method name: {method.Name}");
+
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach(var param in parameters)
+                    {
+                        Console.WriteLine($"Parameter: {param.Name},{param.ParameterType}");
+                    }
+
+                    ConstructorInfo[] constructors = type.GetConstructors(); ;
+                    foreach (var constructor in constructors)
+                    {
+                        Console.WriteLine($"Constructor name: {constructor.Name}");
+                    }
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
